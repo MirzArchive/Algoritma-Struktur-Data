54ef9c0ce7dca4e016d599c142ce4cf36817a0a7
mirza@Regulus <exquisitemirza@student.ub.ac.id>
2023-07-07 20:00:15 +0700

    ``feat(Stack)``: Add final 3rd iteration solution of 3rd hackerrank case

---
+++ practical/assignment3/Solution3_2.java
@@ -0,0 +1,53 @@
+package practical.assignment3;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution3_2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
+        StringBuilder postfix = new StringBuilder();
+
+        char[] infix = in.nextLine().replaceAll("\\s", "").toCharArray();
+        System.out.println(new String(infix));
+        for(char chara : infix) {
+            if (Character.isLetterOrDigit(chara)) {
+                postfix.append(chara);
+            } else if (chara == '(') {
+                stack.add(chara);
+            } else if (chara == ')') {
+                while (!stack.empty() && stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                if (!stack.empty()) {
+                    stack.pop();
+                }
+            } else {
+                if (!stack.empty() && getPrecedence(stack.peek()) >= getPrecedence(chara)) {
+                    postfix.append(stack.pop());
+                }
+                stack.add(chara);
+            }
+        }
+        while (!stack.empty()) {
+            postfix.append(stack.pop());
+        }
+        System.out.println(postfix.toString());
+    }
+
+    private static int getPrecedence(char chara) {
+        switch (chara) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+            default:
+                return -1;
+        }
+    }
+}
