67251bdb24337198f075e2c7821901c49ea299b9
mirza@Regulus <exquisitemirza@student.ub.ac.id>
2023-09-29 11:25:50 +0700

    ``feat(Stack)``: Add submission for 2nd hackerrank case

---
+++ practical/assignment3/Solution2.java
@@ -0,0 +1,51 @@
+package practical.assignment3;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution2 {
+    private static final char[] brackets = {'(', ')', '[', ']', '{', '}'};
+    public static void main(String[] args) {
+        Stack<Character> stack = new Stack<>();
+        Scanner in = new Scanner(System.in);
+        char openingChar;
+
+        char[] characters = in.nextLine().toCharArray();
+        in.close();
+
+        for (char chara : characters) {
+            if (isOpeningBracket(chara)) {
+                stack.add(chara);
+            } else {
+                if (stack.empty()) {
+                    System.out.println("Salah");
+                    return;
+                }
+                openingChar = stack.pop();
+                if (findCorrelatingClosing(openingChar) != chara) {
+                    System.out.println("Salah");
+                    return;
+                }
+            }
+        }
+        System.out.println("Benar");
+    }
+
+    private static boolean isOpeningBracket(char chara) {
+        for (int i = 0; i < brackets.length; i += 2) {
+            if (chara == brackets[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static char findCorrelatingClosing(char opening) {
+        for (int i = 0; i < brackets.length; i += 2) {
+            if (opening == brackets[i]) {
+                return brackets[i + 1];
+            }
+        }
+        return ' ';
+    }
+}
